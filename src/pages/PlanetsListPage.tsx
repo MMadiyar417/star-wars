d2774395422bf2a1bf1dec8ea7c9618ff24a1014
MMadiyar417 <mukhamedyar97@gmail.com>
2024-11-02 11:31:23 +0500

    добавил логику пагинации ?page=1 ?page=2 итд

--- src/pages/PlanetsListPage.tsx
+++ src/pages/PlanetsListPage.tsx
@@ -1,48 +1,45 @@
-// src/pages/PlanetsListPage.tsx
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPlanets } from '../redux/planetsSlice';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { RootState, AppDispatch } from '../redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PlanetsListPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const { planets, loading } = useSelector((state: RootState) => state.planets);
-  const [page, setPage] = React.useState(1);
+  
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('page') || '1', 10);
 
   useEffect(() => {
     dispatch(fetchPlanets(page));
   }, [dispatch, page]);
 
-  if (loading) return <div className="text-center">Загрузка...</div>;
+  if (loading) return <div>Загрузка...</div>;
 
   return (
     <div className="container">
       <h1 className="my-4">Список планет</h1>
+      <div className="mb-4">
+        <Link to="/" className="btn btn-secondary">Назад на главную</Link>
+      </div>
       <ul className="list-group mb-4">
         {planets.map((planet) => (
           <li key={planet.id} className="list-group-item">
-            <Link to={`/planets/details/${planet.id}`} className="text-decoration-none">
-              {planet.name}
-            </Link>
+            <Link to={`/planets/details/${planet.id}`} className="link-primary">{planet.name}</Link>
           </li>
         ))}
       </ul>
       <div className="d-flex justify-content-between">
-        <button 
-          className="btn btn-secondary" 
-          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-        >
-          Назад
-        </button>
-        <button 
-          className="btn btn-primary" 
-          onClick={() => setPage(prev => prev + 1)}
-        >
+        {page > 1 && (
+          <Link to={`/planets?page=${page - 1}`} className="btn btn-secondary">
+            Назад
+          </Link>
+        )}
+        <Link to={`/planets?page=${page + 1}`} className="btn btn-secondary">
           Вперед
-        </button>
+        </Link>
       </div>
     </div>
   );
--- src/pages/StarshipsListPage.tsx
+++ src/pages/StarshipsListPage.tsx
@@ -1,48 +1,45 @@
-// src/pages/StarshipsListPage.tsx
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchStarships } from '../redux/starshipsSlice';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { RootState, AppDispatch } from '../redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StarshipsListPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const { starships, loading } = useSelector((state: RootState) => state.starships);
-  const [page, setPage] = React.useState(1);
+  
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('page') || '1', 10);
 
   useEffect(() => {
     dispatch(fetchStarships(page));
   }, [dispatch, page]);
 
-  if (loading) return <div className="text-center">Загрузка...</div>;
+  if (loading) return <div>Загрузка...</div>;
 
   return (
     <div className="container">
       <h1 className="my-4">Список космических кораблей</h1>
+      <div className="mb-4">
+        <Link to="/" className="btn btn-secondary">Назад на главную</Link>
+      </div>
       <ul className="list-group mb-4">
         {starships.map((starship) => (
           <li key={starship.id} className="list-group-item">
-            <Link to={`/starships/details/${starship.id}`} className="text-decoration-none">
-              {starship.name}
-            </Link>
+            <Link to={`/starships/details/${starship.id}`} className="link-primary">{starship.name}</Link>
           </li>
         ))}
       </ul>
       <div className="d-flex justify-content-between">
-        <button 
-          className="btn btn-secondary" 
-          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-        >
-          Назад
-        </button>
-        <button 
-          className="btn btn-primary" 
-          onClick={() => setPage(prev => prev + 1)}
-        >
+        {page > 1 && (
+          <Link to={`/starships?page=${page - 1}`} className="btn btn-secondary">
+            Назад
+          </Link>
+        )}
+        <Link to={`/starships?page=${page + 1}`} className="btn btn-secondary">
           Вперед
-        </button>
+        </Link>
       </div>
     </div>
   );
